716c306a240f39b7c682a52fd02cebc9eef60e32
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-12-03 11:04:40 -0800

    finally.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-funcs = [func for func in dir(hidden_4) if func[:2] not in "__"]
-for func in funcs:
-    print(func)
+if __name__ == "__main__":
+    import hidden_4
+    funcs = [func for func in dir(hidden_4) if func[:2] not in "__"]
+    for func in funcs:
+        print(func)
