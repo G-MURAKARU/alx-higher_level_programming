a1db6f36e2eae31161adf58dda0312027d5edcb0
G-MURAKARU <85207521+G-MURAKARU@users.noreply.github.com>
2022-12-07 18:24:41 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return
     keys = list(a_dictionary.keys())
     values = list(a_dictionary.values())
