d06a83be59d1f8a9b9a81a3084c5c7fbd058fddf
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-12-07 03:12:34 -0800

    uploaded more data structures project

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return
+    keys = list(a_dictionary.keys())
+    values = list(a_dictionary.values())
+    max_value = max(values)
+    max_value_index = values.index(max_value)
+    max_key = keys[max_value_index]
+    return max_key
