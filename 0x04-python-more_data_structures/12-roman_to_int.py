b0224378471621ec5ab206c98e0cb51b612ae312
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-12-07 04:17:29 -0800

    fixed pycodestyle errors on file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     symbols = {'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500, 
-            'M': 1000,
-            }
+               'V': 5,
+               'X': 10,
+               'L': 50,
+               'C': 100,
+               'D': 500,
+               'M': 1000}
 
     roman_list = list(roman_string)
 
