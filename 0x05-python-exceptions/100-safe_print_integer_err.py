86e33fc4dde80283ad86a472a46a855388b5ac36
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2022-12-19 12:37:40 -0800

    uploaded first advanced task

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as error:
+        sys.stderr.write("Exception: " + str(error) + "\n")
+        return False
