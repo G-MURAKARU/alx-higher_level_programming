5411ce50261bd9a8f88540184b0669c14ab8b12e
Gicheru Murakaru <gicheru.murakaru7@gamil.com>
2023-01-05 01:54:57 +0300

    fixed file 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-
-
 def copy_list(l):
-    """
-    copy_list returns a copy of a list
-
-    Args:
-        l (list): input list to copy
-
-    Returns:
-        list: copy of input list
-    """
-
     return list(l)
