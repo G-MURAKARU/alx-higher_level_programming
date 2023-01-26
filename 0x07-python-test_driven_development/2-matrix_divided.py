78da47427991fa7c5d7802abd1aa61be7a4e30c4
Gicheru Murakaru <gicheru.murakaru7@gamil.com>
2023-01-26 15:53:22 +0300

    updated matrix_divided script

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,29 +2,31 @@
 
 """
     this is the ``2-matrix_divided`` module.
-    the 2-matrix_divided module supplies one function,
+    the 2-matrix_divided module supplies two functions,
+    validate_args(matrix: Any, div: Any) and
     matrix_divided(matrix: list[list[int|float]], div: int|float).
 """
 
 
-def matrix_divided(matrix: list[list], div) -> list[list]:
+def validate_args(matrix, div) -> bool:
     """
-    matrix_divided divides all elements of the input ``matrix`` by the input ``div``
+    validate_args validates input arguments using given criteria
 
     Args:
-        matrix (list[list[int | float]]): input matrix of integers/floating-point numbers
-        div (int | float): input divisor
+        matrix (Any): input matrix
+        div (Any): input integer/floating-point number
 
     Returns:
-        list[list[int|float]]: new matrix of resultant quotients
+        bool: whether arguments are valid or not
     """
 
-    def validate_divisor(div):
+    def validate_divisor():
         """
         validate_divisor validates that ``div`` is a non-zero int/float value
 
-        Args:
-            div (Any): input divisor to validate
+        Raises:
+            TypeError: if ``div`` is neither of type int nor float
+            ZeroDivisionError: if ``div`` is equal to zero
         """
 
         if not isinstance(div, (int, float)):
@@ -32,12 +34,14 @@ def matrix_divided(matrix: list[list], div) -> list[list]:
         elif div == 0:
             raise ZeroDivisionError("division by zero")
 
-    def validate_matrix(matrix):
+    def validate_matrix():
         """
         validate_matrix validates that the input matrix is a list of lists
 
-        Args:
-            matrix (Any): input matrix to validate
+        Raises:
+            TypeError: if matrix if not of type list
+            TypeError: if matrix is an empty list
+            TypeError: if matrix is not strictly a list of lists
         """
 
         # if matrix is not a list
@@ -53,60 +57,67 @@ def matrix_divided(matrix: list[list], div) -> list[list]:
             )
 
         # if matrix is not a list of lists
-        for row in matrix:
-            if not isinstance(row, list):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats"
-                )
+        if not all(isinstance(row, list) for row in matrix):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+            )
 
-    def validate_matrix_elements(matrix):
+    def validate_matrix_dimensions():
         """
-        validate_matrix_elements validates that all matrix elements are of type int/float
+        validate_matrix_dimensions validates that all matrix rows
+        are of the same length
 
-        Args:
-            matrix (list[list[int|float]]): input matrix to validate
-        """
-
-        # if a non-int/non-float element exists
-        for row in matrix:
-            for element in row:
-                if not isinstance(element, (int, float)):
-                    raise TypeError(
-                        "matrix must be a matrix (list of lists) of integers/floats"
-                    )
-
-    def validate_matrix_dimensions(matrix):
-        """
-        validate_matrix_dimensions validates that all matrix rows are of the same length
-
-        Args:
-            matrix (list[list[int|float]]): input matrix to validate
+        Raises:
+            TypeError: if not all matrix rows are if the same length
         """
 
         # length should be same all through, use first row as reference
         length: int = len(matrix[0])
 
         # if any row length differs from first row length
-        for row in matrix:
-            if len(row) != length:
-                raise TypeError("Each row of the matrix must have the same size")
+        if any(len(row) != length for row in matrix):
+            raise TypeError("Each row of the matrix must have the same size")
 
-    validate_divisor(div)
-    validate_matrix(matrix)
-    validate_matrix_dimensions(matrix)
-    validate_matrix_elements(matrix)
+    def validate_matrix_elements():
+        """
+        validate_matrix_elements validates that all matrix elements
+        are of type int/float
 
-    new_matrix: list[list] = []
+        Raises:
+            TypeError: if any matrix element is not of type int/float
+        """
 
-    for row in matrix:
-        new_row: list = []
-        for element in row:
-            new_row.append(round(element / div, 2))
-        new_matrix.append(new_row)
+        # if a non-int/non-float element exists
+        if not all(
+            all(isinstance(ele, (int, float)) for ele in row)
+            for row in matrix
+        ):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+            )
 
-    return new_matrix
+    validate_divisor()
+    validate_matrix()
+    validate_matrix_dimensions()
+    validate_matrix_elements()
+    return True
 
 
-# if __name__ == "__main__":
-#     matrix = [[7, -14, 21], [-3.5, 4.9, 6.3], [9.3, 15, -12.7]]
-#     print(matrix_divided(matrix, 7))
+def matrix_divided(matrix, div) -> list[list]:
+    """
+    matrix_divided divides all elements of the input ``matrix``
+    by the input ``div``
+
+    Args:
+        matrix (list[list[int|float]]): input matrix of
+            integers/floating-point numbers
+        div (int|float): input divisor
+
+    Returns:
+        list[list[int|float]]: new matrix of resultant quotients
+    """
+
+    if validate_args(matrix, div):
+        return [
+            [round(element / div, 2) for element in row] for row in matrix
+        ]
