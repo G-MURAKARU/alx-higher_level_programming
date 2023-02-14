0cdbce7f0885405f082e0cdc37e6a4496b21c260
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2023-02-14 22:36:03 +0300

    fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- group entries with same score from second_table
-SELECT score, COUNT(score) FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
